7b3f52a04caa4d78e76e49380507fcf2d3993974
Perry Tjahya <oioi220419@gmail.com>
2024-11-07 19:07:14 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
 
-const PORT = 4000; //Diusahakan jangan menggunakan PORT 3000 karena biasanya sudah digunakan oleh React
+const express = require("express");
+15
+const mongoose = require("mongoose");
+const User = require("./models/UserSchema");
+const app = express();
+const PORT = 4000; //Diusahakan jangan menggunakan PORT 3000 karena biasanyasudah digunakan oleh React
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb+srv://perry:mongodb@cluster0.0oanl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb.net/test');
+mongoose.connect(
+"mongodb+srv://perry:mongodb@cluster0.0oanl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb.net/test"
+);
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function(){
-console.log('Connected to MongoDB <Nama>');
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+console.log("Connected to MongoDB");
 });
-app.listen(PORT, () => console.log(`Server started at port:${PORT}
-`));
+app.post("/addMahasiswa", async (req, res) => {
+const { name, npm, jurusan } = req.body;
+const user = new User({ name, npm, jurusan });
+try {
+await user.save();
+res
+16
+.status(201)
+.json({ message: "Data Mahasiswa berhasil ditambahkan", data: user });
+} catch (err) {
+res.status(400).send(err);
+}
+});
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
