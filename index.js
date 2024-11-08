ad55416654a5080c696b513b88ba355df0598e0a
Perry Tjahya <oioi220419@gmail.com>
2024-11-08 13:02:27 +0700

    commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require("express");
 15
 const mongoose = require("mongoose");
-const User = require("./models/UserSchema");
+const Buku = require("./Models/BookSchema");
 const app = express();
-const PORT = 4000; //Diusahakan jangan menggunakan PORT 3000 karena biasanyasudah digunakan oleh React
+const PORT = 6942; //Diusahakan jangan menggunakan PORT 3000 karena biasanyasudah digunakan oleh React
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(
@@ -15,14 +15,14 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
 console.log("Connected to MongoDB");
 });
-app.post("/addMahasiswa", async (req, res) => {
-const { name, npm, jurusan, gaji } = req.body;
-const user = new User({ name, npm, jurusan, gaji });
+app.post("/addBook", async (req, res) => {
+const { title, category, author, genre, price } = req.body;
+const buku = new Buku({ title, category, author, genre, price });
 try {
-    await user.save();
+    await buku.save();
         res
         .status(201)
-        .json({ message: "Data Mahasiswa berhasil ditambahkan", data: user });
+        .json({ message: "Data Buku berhasil ditambahkan", data: buku });
     } catch (err) {
 res.status(400).send(err);
 }
